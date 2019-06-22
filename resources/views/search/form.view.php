bd9a32a72436de81d1b662a2e4cd98cd74e536a8
magicandcode <magicandcode@gmail.com>
2019-06-22 23:51:12 +0200

    Improve form view

--- resources/views/search/form.view.php
+++ resources/views/search/form.view.php
@@ -1,12 +1,30 @@
 <?php
-    // todo: Query class to keep track of input values
+    use MarsPhotos\PhotoSearch\Form\SearchForm as Form;
 ?>
-    <form id="search-form">
-        <input name="query_sol" type="number" min="0" max="<?=Rover::getMaxSol()?>" />
-        <select name="query_camera">
-            <?php // todo: Rover cameras method to generate camera options ?>
-            <option value="any">Any Camera</option>
-            <option value="mast">Mast Camera</option>
-        </select>
-        <button type="submit"><?=_('Find Photos')?></button>
-    </form>
+    <section class="container search-form">
+        <header class="header search-form">
+            <h1><?=_('Search Photos')?></h1>
+            <p><?=_('Search photos taken by the Mars rover Curiosity selecting sol (mission day) and camera.')?></p>
+        </header>
+        <form id="search-form" action="." method="<?=Form::getMethod()?>" class="pure-form pure-form-stacked">
+            <fieldset>
+                <label for="sol"><i class="far fa-sun"></i> <?=_('Sol')?></label>
+                <input
+                        name="sol"
+                        value="0"
+                        type="number"
+                        min="0"
+                        max="<?=Form::getMaxSol()?>"
+                        id="sol"
+                />
+                <label for="camera"><i class="fas fa-camera"></i> <?=_('Camera')?></label>
+                <select name="camera" id=""camera">
+                    <?php
+                        Form::getCameraOptions();
+                    ?>
+                </select>
+
+                <button type="submit" class="pure-button"><i class="fas fa-search"></i> <?=_('Find Photos')?></button>
+            </fieldset>
+        </form>
+    </section>
