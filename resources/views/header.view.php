ab672c79904bd022e60e88d7f08fa7bbf79642bf
magicandcode <magicandcode@gmail.com>
2019-06-22 13:26:44 +0200

    Add site header view

---
+++ resources/views/header.view.php
@@ -0,0 +1,12 @@
+<?php
+
+use MarsPhotos\App;
+?>
+<!doctype html5>
+<html>
+<head>
+    <title><?=App::get('app.title')?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
+</head>
+<body>
