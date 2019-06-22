3d056f342ecb190a28a4e8eb2fff0c8def7f02d5
magicandcode <magicandcode@gmail.com>
2019-06-22 22:09:39 +0200

    Add basic styles

--- resources/views/header.view.php
+++ resources/views/header.view.php
@@ -5,8 +5,14 @@ use MarsPhotos\App;
 <!doctype html5>
 <html>
 <head>
+    <meta charset="utf-8">
     <title><?=App::get('app.title')?></title>
+    <link rel="icon" href="<?=App::get('dir.public')?>favicon.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/c81a335880.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
+    <link rel="stylesheet" href="<?=App::get('dir.styles')?>main.css">
 </head>
 <body>
+    <div class="main app">
+        <h1 class="app"><?=_('Mars Rover Photos')?></h1>
