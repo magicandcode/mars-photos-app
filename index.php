cf867bf1295ac260338f92c068a467ce8bd709fc
magicandcode <magicandcode@gmail.com>
2019-06-23 19:50:07 +0200

    Update photos condition

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-// todo: DEBUG only!
-ini_set("xdebug.var_display_max_children", -1);
-ini_set("xdebug.var_display_max_data", -1);
-ini_set("xdebug.var_display_max_depth", -1);
-
 include_once 'bootstrap.php';
 use MarsPhotos\App;
 use MarsPhotos\PhotoSearch\Form\SearchForm as Form;
@@ -13,17 +8,14 @@ use MarsPhotos\Views;
 Views::registerViews(['welcome', 'search/form']);
 
 // Form handling
-if (Form::allValuesSet() && isset($_GET['photos']) && empty($_GET['photos'])) {
-
-    //dd(SearchQuery::get());
+if (Form::allValuesSet() && \sanitized(isset($_GET['photos'])) && \sanitized($_GET['photos']) === 'find') {
     // todo: Photos class accepting stdClass response
-
     // todo: Search and Manifest Request and Response classes?
-    //$manifest = ManifestQuery::get()->response()->manifest();
+
     $photos = SearchQuery::get()->response()->photos();
     $_POST['photos'] = $photos;
     Views::registerViews(['photos/photos.index']);
-} elseif (isset($_GET['photos']) && empty($_GET['photos'])) {
+} elseif (\sanitized(isset($_GET['photos'])) && \sanitized($_GET['photos']) === 'find') {
     Views::registerViews(['error']);
 }
 
