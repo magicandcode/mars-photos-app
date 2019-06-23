03d1292706e9a0ed1069999efbf016ddbb407f11
magicandcode <magicandcode@gmail.com>
2019-06-23 20:19:25 +0200

    Remove console logs

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,5 @@
 // Gets search form
-const searchForm = document.getElementById('search-form');
-console.log('form: ', searchForm); // Checks if we got the form
-
+// const searchForm = document.getElementById('search-form');
 // Gets search form fields
-const formInputs = document.querySelectorAll(`input, select`);
-console.log(formInputs);
-
-const formSubmit = document.querySelector('form button');
-
-//const headers = new Headers();
-
-// Adds eventlistener
-searchForm.addEventListener('submit', (e) => {
-    //e.preventDefault(); // Prevents reloading of page
-});
+// const formInputs = document.querySelectorAll(`input, select`);
+// const formSubmit = document.querySelector('form button');
