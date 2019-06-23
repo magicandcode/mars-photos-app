9ad05f76ca6efd2dd269486ab8b55579eab26925
magicandcode <magicandcode@gmail.com>
2019-06-23 04:17:20 +0200

    Improve getting config options form App:get

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,9 @@ use MarsPhotos\App;
 include_once 'vendor/autoload.php';
 
 // App Container
-App::bind('app', include_once 'config.php');
+App::bind('config', include_once 'config.php');
+App::bind('app', App::get('config')['app']);
+App::bind('api', App::get('config')['api']);
 // todo: set paths relative to root
 App::bind('dir.scripts', '../resources/js/');
 App::bind('dir.styles', '../resources/css/');
