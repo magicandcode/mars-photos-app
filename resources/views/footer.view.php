4f15878547ec86ef6318c863419c72f3dcb58dff
magicandcode <magicandcode@gmail.com>
2019-06-23 19:57:51 +0200

    Improve views

--- resources/views/footer.view.php
+++ resources/views/footer.view.php
@@ -2,9 +2,10 @@
     use MarsPhotos\App;
 ?>
         <footer class="app">
-            <p><a href="https://github.com/magicandcode/">@magicandcode</a></p>
+            <p><a href="https://github.com/magicandcode/" target="_blank">@magicandcode</a></p>
+            <p><a href="https://api.nasa.gov/api.html#MarsPhotos">NASA Mars Rover Photos API</a></p>
         </footer>
     </div>
-<script src="<?=App::get('dir.scripts').'main.js'?>"></script>
+<script src="<?=App::get('dir.scripts').'main.js'?>" target="_blank"></script>
 </body>
 </html>
