4797e826e01fc3306f8326ceec02911b7926ed80
magicandcode <magicandcode@gmail.com>
2019-06-23 12:51:35 +0200

    Prepare for API request

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
+// todo: DEBUG only!
+ini_set("xdebug.var_display_max_children", -1);
+ini_set("xdebug.var_display_max_data", -1);
+ini_set("xdebug.var_display_max_depth", -1);
+
 include_once 'bootstrap.php';
 use MarsPhotos\App;
+use MarsPhotos\PhotoSearch\Api\ManifestQuery;
+use MarsPhotos\PhotoSearch\Form\SearchForm as Form;
+use MarsPhotos\PhotoSearch\Api\SearchQuery;
 use MarsPhotos\Views;
 
 Views::registerViews(['welcome', 'search/form']);
+
+// Form handling
+if (Form::allValuesSet()) {
+    // todo: Photos class accepting Query object
+    // todo: Send response via POST
+
+    // todo: Search and Manifest Request and Response classes?
+    //$photos = SearchQuery::get()->response()->photos();
+    //$manifest = ManifestQuery::get()->response()->manifest();
+}
+
+// Includes views
+App::render();
