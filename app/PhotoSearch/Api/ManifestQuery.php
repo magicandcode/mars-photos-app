0e18c688004e152e8598ec5ce582aa61c029c4dc
magicandcode <magicandcode@gmail.com>
2019-06-23 11:50:10 +0200

    Add abstract query class to enable photo search and manifest requests

---
+++ app/PhotoSearch/Api/ManifestQuery.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MarsPhotos\PhotoSearch\Api;
+
+// Prevent direct access
+use MarsPhotos\App;
+use MarsPhotos\PhotoSearch\Form\CameraTrait;
+use MarsPhotos\PhotoSearch\Form\MethodTrait;
+use MarsPhotos\PhotoSearch\Form\SearchForm as Form;
+
+\debug_backtrace() || die('No.');
+
+if (!\class_exists('ManifestQuery')) {
+    class ManifestQuery extends Query
+    {
+        private $baseUrl = 'https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=DEMO_KEY';
+        private $url;
+
+        private function __construct()
+        {
+            // Check if form has been submitted
+            if (Form::allValuesSet()) {
+                $this->set();
+            }
+        }
+
+        public static function get()
+        {
+            return new ManifestQuery();
+        }
+
+        public function getUrl(): string
+        {
+            return $this->url;
+        }
+
+        private function set(): void
+        {
+            $this->url = $this->baseUrl;
+        }
+
+        private function request(): Request
+        {
+            return Request::get($this);
+        }
+
+        public function response(): Response
+        {
+            return $this->request()->response();
+        }
+    }
+}
