74bccdae302e8662829410f52414d26f3d1e3324
magicandcode <magicandcode@gmail.com>
2019-06-24 22:43:05 +0200

    Add sample config

---
+++ config-sample.php
@@ -0,0 +1,12 @@
+<?php
+// Prevent direct access
+\debug_backtrace() || die('No.');
+
+return [
+    'app' => [
+        'title'  => _('Mars Rover Photos'),
+    ],
+    'api' => [
+        'key' => 'DEMO_KEY'
+    ]
+];
